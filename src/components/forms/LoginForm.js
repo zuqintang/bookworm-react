69ecc091d52d148205215f5f801291b6fc9e1710
zuqintang <zuqintang@gmail.com>
2017-11-28 15:23:04 +0800

    client-side validate

---
+++ src/components/forms/LoginForm.js
@@ -0,0 +1,63 @@
+import React from "react";
+import validator from "validator";
+import { Button, Form } from "semantic-ui-react";
+import InlineError from "../messages/InlineError";
+
+class LoginForm extends React.Component {
+  state = { data: { email: "", password: "" }, errors: {} };
+
+  onChange = e => {
+    this.setState({
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
+  };
+
+  onSubmit = () => {
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+    if (Object.keys(errors).length === 0) {
+      console.log(this.state.data);
+    }
+  };
+
+  validate = data => {
+    const errors = {};
+    if (!data.password) errors.password = "Can't be blank";
+    if (!validator.isEmail(data.email)) errors.email = "Invalid email";
+    return errors;
+  };
+  render() {
+    const { data, errors } = this.state;
+    return (
+      <Form onSubmit={this.onSubmit}>
+        <Form.Field error={!!errors.email}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="example@example.com"
+            onChange={this.onChange}
+            value={data.email}
+          />
+          {errors.email && <InlineError text={errors.email} />}
+        </Form.Field>
+        <Form.Field error={!!errors.password}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="make it secure"
+            onChange={this.onChange}
+            value={data.password}
+          />
+          {errors.password && <InlineError text={errors.password} />}
+        </Form.Field>
+        <Button primary>Login</Button>
+      </Form>
+    );
+  }
+}
+
+export default LoginForm;
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,8 +1,10 @@
 import React from "react";
+import LoginForm from "../forms/LoginForm";
 
 const LoginPage = () => (
   <div>
     <h1>Login Page</h1>
+    <LoginForm />
   </div>
 );
 
